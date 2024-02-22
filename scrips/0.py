ea4de04d35130c2479c9d5346b30cf8cd8456a57
jdmantillaq <jdmantillaq@unal.edu.co>
2024-02-22 17:21:58 -0500

    Imported libraries

--- scrips/0.py
+++ scrips/0.py
@@ -1,1 +1,18 @@
-%
+#%%
+"""
+Created on 22-Feb-2024
+@author: jdmantillaq
+"""
+
+import plotly.express as px
+import pandas as pd
+import numpy as np
+
+from dash import Dash, dcc, html, dash_table
+import dash_bootstrap_components as dbc
+from dash.dependencies import Output, Input
+from dash.exceptions import PreventUpdate
+from dash_bootstrap_templates import load_figure_template
+
+#%%
+
